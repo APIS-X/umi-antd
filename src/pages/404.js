f6f2090693a50059aaf55ff4562b7b3fea263fd0
APIS-X <971888462@qq.com>
2021-07-09 10:22:03 +0800

    what：code update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,10 @@
+import React, {useEffect} from 'react';
+
 export default function Template() {
+  useEffect(() => {
+    console.log('location', location);
+  }, [location]);
+
   return (
     <div>
       <h1>404~ 抱歉，暂无权限查看该页面或者页面不存在~</h1>
