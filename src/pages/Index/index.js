a4227e33a0cafed25b89f4b378803036766600de
APIS-X <971888462@qq.com>
2021-07-16 09:44:56 +0800

    what: 新增Layer组件，扩展AuthBlock组件

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -3,12 +3,8 @@ import React, {memo, useState, useEffect} from 'react';
 import {Form, Button, Input} from 'antd';
 
 import ModCard from '@/components/ModCard';
-import Layer from '../../components/Layer';
 
 const Template = (props) => {
-  console.log('layer', Layer);
-  Layer.show();
-
   return <ModCard title="首页"></ModCard>;
 };
 
